d957b84fb862ea0b2e78d298fec24489828a8f95
Jorge <glph8911@gmail.com>
2023-04-13 15:15:41 -0500

    creating game logic

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default function Square({children, updateBoard, index}) {
-  return (
-    <div className='square'>{children}</div>
-  )
+export default function Square({ children, isSelected, updateBoard, index }) {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick = () => { updateBoard(index) }
+  return <div onClick={handleClick} className={className}>{children}</div>;
 }
