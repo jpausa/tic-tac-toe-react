3dbca1b6e8681db40e7100a7d9066f7cecbb9ae5
Jorge <glph8911@gmail.com>
2023-04-14 12:32:40 -0500

    creating draw logic

--- src/App.jsx
+++ src/App.jsx
@@ -44,16 +44,28 @@ function App() {
     setBoard(newBoard);
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+    }else if(checkEndGame(newBoard)){
+      setWinner(false)
     }
+  };
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
   };
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Restart game</button>
       <section className="game">
         {board.map((_, index) => {
           return (
@@ -67,6 +79,19 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Draw" : "Win"}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Restart game</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
