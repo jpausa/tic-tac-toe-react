55a1fa2e90d058949d48eec74c7488f98ebcd5eb
Jorge <glph8911@gmail.com>
2023-04-18 09:36:12 -0500

    using localStorage to save game status

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,18 @@ import { Board } from "./components/Board";
 import { Turns } from "./components/Turns";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
+
   const [winner, setWinner] = useState(null);
 
   const updateBoard = (index) => {
@@ -18,6 +28,8 @@ function App() {
     setBoard(newBoard);
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       confetti();
@@ -31,6 +43,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   return (
